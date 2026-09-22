99f910a8e7fe4871f0012018cca314e080c0170c
Adeel Ahmad <adeelahmad99@gmail.com>
2026-09-22 17:32:49 +1000

    chore: scaffold S3-10 T7d stubs

--- internal/daemon/lock.go
+++ internal/daemon/lock.go
@@ -12,6 +12,9 @@ import (
 	"github.com/adeelahmad/snapback/internal/errcode"
 )
 
+// lockFunc is the seam tests use to count lock acquisitions.
+var lockFunc = Lock
+
 // Lock takes the single-instance lock in stateDir.
 func Lock(stateDir string) (unlock func(), err error) {
 	f, err := os.OpenFile(filepath.Join(stateDir, "daemon.lock"), os.O_RDWR|os.O_CREATE, 0o600)
